865c1c2513c660929f3760eba47652eeed23aba0
Robert Lichtenberger <r.lichtenberger@synedra.com>
2020-11-18 08:53:53 +0100

    Demo expanded.

--- src/test/java/net/synedra/validatorfx/demo/ValidatorFXDemo.java
+++ src/test/java/net/synedra/validatorfx/demo/ValidatorFXDemo.java
@@ -8,6 +8,7 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.binding.StringBinding;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -21,7 +22,10 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import net.synedra.validatorfx.Check;
+import net.synedra.validatorfx.Decoration;
 import net.synedra.validatorfx.DefaultDecoration;
+import net.synedra.validatorfx.ValidationMessage;
 import net.synedra.validatorfx.Validator;
 
 /** ValidatorFXDemo demonstrates the features of ValidatorFX.
@@ -41,21 +45,16 @@ public class ValidatorFXDemo extends Application {
 		
 		Text sceneTitle = new Text("Welcome");
 		sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		
-		TextField userTextField = new TextField();
-		
-		PasswordField password = new PasswordField();
-		PasswordField passwordConfirmation = new PasswordField();
-		
-		TextArea problems = createProblemOutput();
+		grid.add(sceneTitle, 0, 0, 2, 1);
 		
 		Button signUp = new Button("Sign up");
-		signUp.disableProperty().bind(validator.containsErrorsProperty());
+		signUp.disableProperty().bind(validator.containsErrorsProperty());		
 		HBox bottomBox = new HBox(10);
 		bottomBox.setAlignment(Pos.BOTTOM_RIGHT);
 		bottomBox.getChildren().add(signUp);
 		signUp.setOnAction(e -> signUp());
 		
+		TextField userTextField = new TextField();
 		validator.createCheck()
 			.withMethod(c -> {
 				String userName = c.get("username");
@@ -67,7 +66,12 @@ public class ValidatorFXDemo extends Application {
 			.decorates(userTextField)
 			.immediate();
 		;
+		grid.add(new Label("User Name:"), 0, 1);
+		grid.add(userTextField, 1, 1);
 		
+		
+		PasswordField password = new PasswordField();
+		PasswordField passwordConfirmation = new PasswordField();
 		validator.createCheck()
 			.withMethod(c -> {
 				if (!c.get("password").equals(c.get("passwordConfirmation"))) {
@@ -80,7 +84,6 @@ public class ValidatorFXDemo extends Application {
 			.decorates(passwordConfirmation)
 			.immediate()
 		;
-		
 		validator.createCheck()
 			.withMethod(c -> {
 				String pwd = c.get("password");
@@ -92,22 +95,68 @@ public class ValidatorFXDemo extends Application {
 			.decorates(signUp)
 			.immediate()
 		;
-		
-		grid.add(sceneTitle, 0, 0, 2, 1);
-		grid.add(new Label("User Name:"), 0, 1);
-		grid.add(userTextField, 1, 1);
 		grid.add(new Label("Password:"), 0, 2);
 		grid.add(password, 1, 2);
 		grid.add(new Label("Password confirmation:"), 0, 3);
 		grid.add(passwordConfirmation, 1, 3);
-		grid.add(problems, 0, 4, 2, 1);
-		grid.add(bottomBox, 1, 6);
+		
+		TextField summand1 = new TextField("17");
+		TextField summand2 = new TextField("4");
+		TextField sum = new TextField("21");
+		Label check = new Label(""); check.setMinWidth(200);
+		
+		validator.createCheck()
+			.dependsOn("s1", summand1.textProperty())
+			.dependsOn("s2", summand2.textProperty())
+			.dependsOn("sum", sum.textProperty())
+			.withMethod(this::checkSum)
+			.decoratingWith(this::sumDecorator)
+			.decorates(check)
+			.immediate()
+		;
+		
+		HBox hb = new HBox(5);
+		hb.setAlignment(Pos.BASELINE_LEFT);
+		hb.getChildren().addAll(summand1, new Text("+"), summand2, new Text("="), sum, check);
+		grid.add(hb, 0, 4, 2, 1);
+
+		TextArea problems = createProblemOutput();		
+		grid.add(problems, 0, 5, 2, 1);
+		
+		grid.add(bottomBox, 1, 7);
 		
 		Scene scene = new Scene(grid);
 		scene.getStylesheets().add(getClass().getResource("demo.css").toExternalForm());
 		
 		primaryStage.setScene(scene);		
 		primaryStage.show();		
+	}
+	
+	private void checkSum(Check.Context c) {
+		try {
+			int s1 = Integer.parseInt(c.get("s1"));
+			int s2 = Integer.parseInt(c.get("s2"));
+			int sum = Integer.parseInt(c.get("sum"));
+			if (s1 + s2 != sum) {
+				c.error("sum should be " + (s1 + s2));
+			}
+		} catch (NumberFormatException e) {
+			c.error("number format");
+		}
+	}
+	
+	private Decoration sumDecorator(ValidationMessage m) {
+		return new Decoration() {
+			@Override
+			public void remove(Node target) {
+				((Label) target).setText("OK");
+			}
+			
+			@Override
+			public void add(Node target) {
+				((Label) target).setText("ERR - " + m.getText());
+			}
+		};
 	}
 
 	private TextArea createProblemOutput() {
